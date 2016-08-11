681a25dc1f715fc3324d47f7dc7a9198bac23bf3
Kumar Ayush <cheekujodhpur@gmail.com>
2016-08-11 19:42:07 +0530

    added assignment basecode

---
+++ a1/mydraw_class.hpp
@@ -0,0 +1,32 @@
+#ifndef _MYDRAW_CLASS_HPP_
+#define _MYDRAW_CLASS_HPP_
+
+//Define all classes like the color class, adding appropriate methods and data members. 
+//Implementation of the methods go into the corresponding cpp file
+
+//------------------------
+//color_t class
+
+class color_t {
+private:
+  float r,g,b;
+public:
+  color_t();
+  color_t(const float _r, const float _g, const float _b);
+
+  void set(const float _r, const float _g, const float _b);
+  float R(void);
+  float G(void);
+  float B(void);
+};
+
+//------------------------
+
+//point_t class
+class point_t {
+
+};
+
+//------------------------
+
+#endif
